8dc4a8f40a3bba5d84fd68fc257b98b78f46e528
Lotus <heningtonfrota@gmail.com>
2021-08-27 18:41:59 -0400

    Hierarquia de Componentes

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,16 +1,21 @@
 <template>
-    <PrimeiroCompomente />
-    <LifeCycle />
+    <div>
+        <PrimeiroCompomente />
+        <LifeCycle />
+        <Pessoa />
+    </div>
 </template>
 
 <script>
     import PrimeiroCompomente from './components/PrimeiroComponente.vue'
     import LifeCycle from './components/LifeCycle.vue'
+    import Pessoa from './components/Pessoa.vue'
     export default {
         name: 'App',
         components: {
             PrimeiroCompomente,
-            LifeCycle
+            LifeCycle,
+            Pessoa
         }
     }
 </script>
