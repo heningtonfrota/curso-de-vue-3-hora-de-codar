2b6d8101c93129d2e0a48b1e7bc1b55b8a8521e5
Lotus <heningtonfrota@gmail.com>
2021-08-27 16:59:34 -0400

    Inputs e Data Binding

--- 1_instalacao_vue/src/app.js
+++ 1_instalacao_vue/src/app.js
@@ -1,10 +1,18 @@
 const MyNameApp = {
     data() {
         return {
-            name: "Henington",
-            age: "26"
+            name: "",
+            age: "26",
+            input_name: ""
         }
-    }
+    },
+    methods: {
+        submitForm(e) {
+            e.preventDefault();
+            console.log(this.input_name);
+            this.name = this.input_name;
+        }
+    },
 }
 
 Vue.createApp(MyNameApp).mount("#app");
