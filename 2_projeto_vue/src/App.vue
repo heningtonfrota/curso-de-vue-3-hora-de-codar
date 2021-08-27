68846d9255368f82f8509f8701393807f66a1771
Lotus <heningtonfrota@gmail.com>
2021-08-27 18:18:16 -0400

    Life Cycle Hooks

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,13 +1,16 @@
 <template>
     <PrimeiroCompomente />
+    <LifeCycle />
 </template>
 
 <script>
     import PrimeiroCompomente from './components/PrimeiroComponente.vue'
+    import LifeCycle from './components/LifeCycle.vue'
     export default {
         name: 'App',
         components: {
-            PrimeiroCompomente
+            PrimeiroCompomente,
+            LifeCycle
         }
     }
 </script>
---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            nome: 'Ainda não sei'
+        }
+    },
+    created() {
+        setTimeout(() => {
+            this.nome = "Henington"
+        }, 1000);
+    },
+    mounted() {
+        setTimeout(() => {
+            this.nome = "Aline"
+        }, 2000);
+    },
+}
+</script>
+
+<style>
+
+</style>
