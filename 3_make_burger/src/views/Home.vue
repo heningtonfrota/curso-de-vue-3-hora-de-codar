6ecf653ec9b89cbd34bab5163760116f0ddeb7b4
Lotus <heningtonfrota@gmail.com>
2021-08-30 13:22:00 -0400

    Criando banner da aplicação

--- 3_make_burger/src/views/Home.vue
+++ 3_make_burger/src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
+    <Banner />
     <div class="main-container">
-        <h1>Home</h1>
+        <h1>Monte o seu burger: </h1>
     </div>
 </template>
 
 <script>
+import Banner from '../components/Banner.vue';
 export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+        Banner
+    }
 }
 </script>
