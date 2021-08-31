beb96272430068fde0f3d4f59f97b3bf4cf0274f
Lotus <heningtonfrota@gmail.com>
2021-08-31 14:44:43 -0400

    Criando formulário de cadastro

--- 3_make_burger/src/views/Home.vue
+++ 3_make_burger/src/views/Home.vue
@@ -2,15 +2,18 @@
     <Banner />
     <div class="main-container">
         <h1>Monte o seu burger: </h1>
+        <BurgForm />
     </div>
 </template>
 
 <script>
 import Banner from '../components/Banner.vue';
+import BurgForm from '../components/BurgForm.vue';
 export default {
     name: 'Home',
     components: {
-        Banner
+        Banner,
+        BurgForm
     }
 }
 </script>
